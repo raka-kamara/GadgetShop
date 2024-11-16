d3e574739d8c05add60084d5b31a5e30319340a2
raka-kamara <rakakamara@outlook.com>
2024-11-16 16:29:13 +0600

    pages created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import ContactUs from "../Pages/ContactUs";
+import About from "../Pages/About";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +13,19 @@ export const router = createBrowserRouter([
         {
           path:"/",
           element:<Home/>,
-        }
+        },
+        {
+          path:"/products",
+          element:<Products/>,
+        },
+        {
+          path:"/contact-us",
+          element:<ContactUs/>,
+        },
+        {
+          path:"/about",
+          element:<About/>,
+        },
       ]
     },
   ]);
